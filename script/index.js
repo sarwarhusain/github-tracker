2a6987f0c219a99b25fa5e1c0f5d212318cce8f1
Sarwar Hussain <sarwarahmed693@gmail.com>
2026-03-07 02:46:24 +0600

    added Nav Bar

---
+++ script/index.js
@@ -0,0 +1,14 @@
+// loginPage
+// function authentication() {
+//   const userName = document.getElementById("userName").value;
+//   const password = document.getElementById("password").value;
+
+//   if (userName === "admin" && password === "admin123") {
+//     document.getElementById("loginPage").classList.add("hidden");
+//     document.getElementById("homePage").classList.remove("hidden");
+//   } else {
+//     alert("invalid");
+//   }
+// }
+// loginPage();
+//
