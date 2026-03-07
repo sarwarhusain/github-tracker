e581242b593d81fe4091bc6ccdd07ab82f2ec81a
Sarwar Hussain <sarwarahmed693@gmail.com>
2026-03-08 01:40:24 +0600

    design card and also added card border, toggeling

--- script/index.js
+++ script/index.js
@@ -16,81 +16,97 @@
 // };
 //
 let issues = [];
+const count = document.getElementById("count");
+const createLabels = (arr) => {
+  return arr
+    .map(
+      (el) =>
+        `<span class="btn btn-sm border-b-cyan-600 m-1 bg-[#FFF8DB]">${el}</span>`,
+    )
+    .join("");
+};
 
-const loadAllIssues = () => {
-  fetch("../public/public.json")
+const loadAllIssues = (id) => {
+  fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
     .then((res) => res.json())
     .then((data) => {
       issues = data.data;
+      count.innerText = issues.length;
+      displayCard(issues);
       displayCardBtn(issues);
     });
 };
-const loadAllCards = () => {
-  fetch("../public/public.json")
-    .then((res) => res.json())
-    .then((data) => {
-      displayCard(data.data);
-    });
-};
-//display Card
-const loadCard = (id) => {
-  fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
-    .then((res) => res.json())
-    .then((data) => {
-      displayCard(data);
-    });
-};
-//showOpen close
 
-const showOpen = () => {
+// const loadCard = (id) => {
+//   fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       displayCard(data);
+//     });
+// };
+//button toggling
+
+const toggle = (id) => {
+  document.getElementById("btnAll").classList.remove("btn-primary");
+  document.getElementById("btnOpen").classList.remove("btn-primary");
+  document.getElementById("btnClosed").classList.remove("btn-primary");
+
+  document.getElementById(id).classList.add("btn-primary");
+};
+
+const allTab = () => {
+  count.innerText = issues.length;
+  toggle("btnAll");
+  displayCard(issues);
+};
+const openTab = () => {
   const statusOpen = issues.filter((issue) => issue.status === "open");
+  count.innerText = statusOpen.length;
+  toggle("btnOpen");
   displayCard(statusOpen);
-  console.log(statusOpen);
 };
-const showClose = () => {
+const closeTab = () => {
   const statusClose = issues.filter((issue) => issue.status === "closed");
-
+  count.innerText = statusClose.length;
+  toggle("btnClosed");
   displayCard(statusClose);
-  console.log(statusClose);
 };
 
-// assignee: "";
-// author: "shortcuts_shawn";
-// createdAt: "2024-01-30T10:15:00Z";
-// description: "Implement keyboard shortcuts for common actions to improve productivity for power users.";
-// id: 27;
-// labels: ["enhancement"];
-// priority: "low";
-// status: "open";
-// title: "Add keyboard shortcuts";
-// updatedAt: "2024-01-30T10:15:00Z";
-const displayCard = (issuesCards) => {
+const displayCard = (items) => {
   const cardContainer = document.getElementById("cardContainer");
   cardContainer.innerHTML = "";
-  issuesCards.forEach((issueCard) => {
+  items.forEach((item) => {
+    const colorStatus =
+      item.status === "open"
+        ? "border-t-4 border-green-500"
+        : "border-t-4 border-purple-500";
+
     const cardDiv = document.createElement("div");
-    cardDiv.innerHTML = `<div class="card bg-base-100  shadow-sm">
-            <div class="card-body">
+
+    cardDiv.innerHTML = `
+    
+         <div class=" bg-base-100 md:w-72 lg:h-full shadow-sm rounded-lg ${colorStatus}">
+            <div class="card-body ">
               <div class="flex justify-between">
-                <button>round</button>
-                <button>high</button>
+                <button class="rounded-full m-2 btn border-t-cyan-600">Right</button>
+                <button class="rounded-full m-2 btn border-t-cyan-600">${item.priority}</button>
               </div>
-              <h2 class="card-title">${issueCard.title}</h2>
-              <p>
-                A card component has a figure, a body part, and inside body
-                there are title and actions parts
+              <h2 class="card-title text-[14px]">${item.title}</h2>
+              <p class="text-[12px]">
+                ${item.description}
               </p>
               <div class="card-actions gap-4 justify-between">
-                <div class="badge badge-outline">Fashion</div>
-                <div class="badge badge-outline">Products</div>
+                <div class="text-[12px] ">${createLabels(item.labels)}</div>
               </div>
-              <div class="card-actions gap-4 justify-between">
-                <div class="">#1 by john_doe</div>
-                <div class="">created art</div>
+              <div class="divider"></div>
+              <div class="text-[12px]  grid grid-cols-12 gap-4 justify-between">
+                 <div class="col-span-6">#${item.id} by ${item.author}</div>
+                <div class="col-span-6">${new Date(item.createdAt).toLocaleDateString()}</div>
               </div>
-              <div class="card-actions gap-4 justify-between">
-                <div class="">1/15/2024</div>
-                <div class="">updated art</div>
+               
+              <div class="card-actions text-[12px]  gap-4 justify-between">
+                <div class="">${item.assignee ? item.assignee : "No Assignee"}</div>
+                <div class="">${new Date(item.updatedAt).toLocaleDateString()}</div>
               </div>
             </div>
           </div>`;
@@ -103,17 +119,18 @@ const displayCardBtn = (issues) => {
   btnContainer.innerHTML = "";
   console.log(issues);
   const btnAll = document.createElement("div");
-  btnAll.innerHTML = ` <button onclick="loadAllCards()" class="btn btn-soft btn-primary ">
-      All
-    </button>
-    <button onclick="showOpen()" class="btn btn-soft btn-primary ">
-      open
-    </button><button onclick="showClose()" class="btn btn-soft btn-primary ">
-      close
-    </button>
+  btnAll.classList.add("space-x-10");
+  btnAll.innerHTML = `
+                  <button id="btnAll" onclick="allTab()" class="btn  btn-primary ">
+                    All </button>
+                  <button id="btnOpen" onclick="openTab()" class=" btn btn-primary ">
+                    open
+                  </button>
+                  <button id="btnClosed" onclick="closeTab()" class="btn btn-primary  ">
+                    close
+                  </button>
     `;
 
   btnContainer.appendChild(btnAll);
 };
 loadAllIssues();
-loadAllCards();
